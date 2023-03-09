3fe58b068e04c56026fadd34581a0f34bf8d85b1
Andrii Spiian <andrey.spiyan@icloud.com>
2023-03-09 21:21:44 +0300

    js prompt. alert

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let a = parseInt(prompt("Введите первое число: "));
+let b = parseInt(prompt("Введите второе число: "));
+let c = parseInt(prompt("Введите третье число: "));
+let sum;
+
+sum = (a+b+c)/3;
+alert(sum);
+
+// if((b<a && a<c) || (c<a && a<b)) alert(a);
+// if((a<b && b<c) || (c<b && b<a)) alert(b);
+// if((a<c && c<b) || (b<c && c<a)) alert(c);
+
+
